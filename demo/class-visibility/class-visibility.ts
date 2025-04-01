48357f23ad844403e4d3724824fe71906a1f3c9e
Jonathan(Khonsu) <jonathan@ankhstudio.com>
2025-04-01 11:54:48 -0500

    completed classes and class visibility demos

--- demo/class-visibility/class-visibility.ts
+++ demo/class-visibility/class-visibility.ts
@@ -15,3 +15,68 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/classes.html#member-visibility
 //
+
+class Person {
+    // `name` is accessible anywhere:
+    public name: string;
+    // `email` is accessible in `Person` and it's subclasses:
+    protected email: string;
+    // `age` is accessible on ly in `Person`:
+    private age: number;
+    
+    constructor(name: string, age: number, email: string) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // `greet` is accessible anywhere:
+    public greet(): void {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+
+    // `getEmail` is accessible in `Person` and it's subclasses:
+    protected getEmail(): string {
+        return this.email;
+    }
+
+    // `getAge` is accessible only in `Person`:
+    private getAge(): number {
+        return this.age;
+    }
+}
+
+// Use `Person` as a subclass
+class Employee extends Person {
+    public department: string;
+
+    constructor(name: string, age: number, email: string, department: string) {
+        super(name, age, email);
+        this.department = department;
+    }
+
+    public getEmployeeInfo(): string {
+        return `Name: $this.name}, Email: ${this.getEmail()}, Age: not accessible, Department: ${this.department}`;
+    }
+
+    // Make the meail publicly accessible by forwarding the function call:
+    public override getEmail(): string {
+        // Use the parent class implementation of `getEmail` through `super`. Even
+        // though `getEmail` is marked as protected, we can still expose the data
+        // publicly because the `Employee` class has permission to access `getEmail`.
+        return super.getEmail();
+    }
+}
+
+const tommy = new Person("Tommy", 21, 'tommy@example.com');
+assert.equal(tommy.name, "Tommy");
+
+const jason = new Employee("Jason", 22, "jason@example.com", "Tyranosaurus");
+assert.equal(jason.name, "Jason");
+assert.equal(jason.department, "Tyranosaurus");
+assert.equal(jason.getEmail(), "jason@example.com");
+assert.equal(
+    jason.getEmployeeInfo(),
+    "Name: Jason, Email: jason@example.com, Age: not accessible, Department: Tyranosaurus"
+);
+// assert.equal(jason.getAge(), 22); // Erro: Property `age` is private and only accessible within class `Person`.
--- demo/classes/classes.ts
+++ demo/classes/classes.ts
@@ -9,3 +9,79 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/classes.html
 //
+
+
+// To create a class definition, we use the `class` keyword followed
+// by the name of the object and then curly braces:
+class Empty {}
+
+// Clases can have `properties` and they must have default values set.
+// These defaults will be used when the object is created.
+class Color {
+    // Always include the type annotation. It's optionall, but
+    // readability will be enhanced with the type specified.
+    r = 0;
+    g = 0;
+    b = 0; // Not so great.
+}
+
+// We can create a new object using the `new` keyword. Creating
+// a new object is called `instantiation`.
+const c = new Color(); // Color { r: 0, g: 0, b: 0 }
+
+// We use dot notation to set values within the object:
+c.r = 255; // Color { r: 255, g: 0, b: 0 }
+
+// And we can read values using dot notation as well;
+const redValue = c.r;
+
+// Sometimes we may want to perform additional actions
+// when instantiating a new class. To do so, we can use 
+// a class contructor:
+class Dimension {
+    // We aren't specifying the defaults directly on the properties.
+    // We will set the defaults in the constructor instead.
+    length: number;
+    width: number;
+    height: number;
+
+    // Class constructor is a function that runs each time we create
+    // a Dimension using the `new` keyword. Constructors always return
+    // the type they are defined in (`Dimension` in this case). So
+    // there is no need for the return type annotation.
+    constructor() {
+        // Each time we make a new Dimension, this message will be logged.
+        console.log("created a new default Dimension");
+
+        // Since we didn't specify the defaults, we _must_ do so here. Failure
+        // to specify defaults will result in an error.
+        this.length = 1;
+        this.width = 1;
+        this.height = 1;
+    }
+}
+
+const one = new Dimension(); // 'created a new Dimension'
+
+class Point {
+    x: number;
+    y: number;
+
+    // We can also write the constructor with function parameters. This
+    // allows us to easiy set values during creation.
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // We can create 'class emthods' wich can be called using dot notation.
+    // This method moves the point by the provided amount.
+    translate(x: number, y: number) {
+        this.x += x;
+        this.y += y;
+    }
+}
+
+const a = new Point(5, 5); // Point {x: 5, y: 5 }
+a.translate(-5, -5); // Point { x: 0, y: 0 }
+assert.deepEqual([a.x, a.y], [0, 0]);
