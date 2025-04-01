f5ca52d3e48bf70e2f76758d95cf18c02f5d91d5
Jonathan(Khonsu) <jonathan@ankhstudio.com>
2025-04-01 11:54:05 -0500

    completed array objects demo

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,3 +1,27 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+import { title } from "process";
 
+// Arrays can contain anything, including objects.
+type Link = {
+    title: string;
+    url: string;
+};
+
+const microsoft = {
+    title: "microsoft",
+    url: "https://www.microsoft.com",
+};
+
+const typescript = {
+    title: "typescript",
+    url: "https://www.typescriptlang.org",
+};
+
+const sites = [microsoft, typescript];
+console.log(sites);
+
+const tsUrl = sites[1].url;
+sites[0].title = "Microsoft";
+
+console.log(sites);
